50a320e6aa6b13014501c9fb4f553cb371fe31d5
Peter Kang <peeterkang@gmail.com>
2022-03-08 15:13:42 -0800

    feat: navigation/route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import CreatePost from './components/CreatePost';
 
 function App() {
   return (
     <Router>
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/create-post' element={<CreatePost />}/>
+      </Routes>
     </Router>
   );
 }
