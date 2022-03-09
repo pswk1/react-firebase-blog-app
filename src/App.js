f26465f16918bf13d3cef54eebc6962e5ebcaf85
Peter Kang <peeterkang@gmail.com>
2022-03-09 13:40:43 -0800

    feat: responsive navbar & MUI setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import CreatePost from './components/CreatePost';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+      {/* <nav>
+        <Link to='/' >Home</Link>
+        <Link to='/create-post' >Create Post</Link>
+        <Link to='/login' >Login</Link>
+      </nav> */}
+      <Navbar />
       <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/login' element={<Login />}/>
