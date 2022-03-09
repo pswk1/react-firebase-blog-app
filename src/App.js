d3ef2760daf0d5328aebe8e70e6c31d333ee4a25
Peter Kang <peeterkang@gmail.com>
2022-03-09 13:51:52 -0800

    refactor(nav): remove unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import CreatePost from './components/CreatePost';
@@ -8,11 +8,6 @@ import Navbar from './components/Navbar'
 function App() {
   return (
     <Router>
-      {/* <nav>
-        <Link to='/' >Home</Link>
-        <Link to='/create-post' >Create Post</Link>
-        <Link to='/login' >Login</Link>
-      </nav> */}
       <Navbar />
       <Routes>
       <Route path='/' element={<Home />}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,6 @@ import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 
@@ -19,11 +17,9 @@ const pages = [
 	{ text: 'Login', to: '/login' },
 ];
 
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-const ResponsiveAppBar = () => {
+const Navbar = () => {
 	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [setAnchorElUser] = React.useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -35,10 +31,6 @@ const ResponsiveAppBar = () => {
 	const handleCloseNavMenu = () => {
 		setAnchorElNav(null);
 	};
-
-	// const handleCloseUserMenu = () => {
-	// 	setAnchorElUser(null);
-	// };
 
 	return (
 		<AppBar position='static'>
@@ -110,37 +102,13 @@ const ResponsiveAppBar = () => {
 					</Box>
 
 					<Box sx={{ flexGrow: 0 }}>
-						{/* <Tooltip title='Open settings'> */}
 							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
 								<Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
 							</IconButton>
-						{/* </Tooltip> */}
-						{/* <Menu
-							sx={{ mt: '45px' }}
-							id='menu-appbar'
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map((setting) => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
-									<Typography textAlign='center'>{setting}</Typography>s
-								</MenuItem>
-							))}
-						</Menu> */}
 					</Box>
 				</Toolbar>
 			</Container>
 		</AppBar>
 	);
 };
-export default ResponsiveAppBar;
+export default Navbar;
