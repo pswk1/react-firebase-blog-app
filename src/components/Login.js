d61191364cc913a79bacfc7a39dc6bc0e1031424
Peter Kang <peeterkang@gmail.com>
2022-03-09 19:02:35 -0800

    refactor: login page with MUI components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,43 @@
 import React from 'react';
 import { auth, provider } from '../firebase-config';
 import { signInWithPopup } from 'firebase/auth';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import GoogleButton from 'react-google-button';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const Login = ({ setIsAuth }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	const signInWithGoogle = () => {
 		signInWithPopup(auth, provider)
 			.then((result) => {
-        localStorage.setItem('isAuth', true);
-        setIsAuth(true);
+				localStorage.setItem('isAuth', true);
+				setIsAuth(true);
 				navigate('/');
 			})
 			.catch((error) => {
-        console.log(error);
+				console.log(error);
 			});
 	};
 	return (
-		<div className='loginPage'>
-			<p>Sign In With Google to Continue</p>
-			<GoogleButton onClick={signInWithGoogle} >Sign In With Google</GoogleButton>
-		</div>
+		<Grid
+			container
+			direction='column'
+			justifyContent='center'
+			alignItems='center'
+			padding={6}
+		>
+			<Grid item marginBottom={5}>
+				<Typography noWrap variant='h4'>Sign In With Google to Continue</Typography>
+			</Grid>
+
+			<Grid item>
+				<GoogleButton onClick={signInWithGoogle}>
+					Sign In With Google
+				</GoogleButton>
+			</Grid>
+		</Grid>
 	);
 };
 
