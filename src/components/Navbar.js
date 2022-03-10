18cd396d9fc2ceaceb022bac9f2dc064eb3cca44
Peter Kang <peeterkang@gmail.com>
2022-03-09 18:38:47 -0800

    refactor: move logout to avatar tooltip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
@@ -10,11 +9,12 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import MenuItem from '@mui/material/MenuItem';
+import Tooltip from '@mui/material/Tooltip';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ isAuth, signUserOut }) => {
 	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [setAnchorElUser] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -25,6 +25,10 @@ const Navbar = ({ isAuth, signUserOut }) => {
 
 	const handleCloseNavMenu = () => {
 		setAnchorElNav(null);
+	};
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
 	};
 
 	return (
@@ -85,7 +89,7 @@ const Navbar = ({ isAuth, signUserOut }) => {
 									</Link>
 								</MenuItem>
 							)}
-							{!isAuth ? (
+							{!isAuth && (
 								<MenuItem onClick={handleCloseNavMenu}>
 									<Link to='/login'>
 										<Typography color='primary' textAlign='center'>
@@ -93,10 +97,6 @@ const Navbar = ({ isAuth, signUserOut }) => {
 										</Typography>
 									</Link>
 								</MenuItem>
-							) : (
-								<Button variant='contained' onClick={signUserOut}>
-									Log Out
-								</Button>
 							)}
 						</Menu>
 					</Box>
@@ -118,21 +118,41 @@ const Navbar = ({ isAuth, signUserOut }) => {
 							</Link>
 						)}
 
-						{!isAuth ? (
+						{!isAuth && (
 							<Link to='/login'>
 								<Typography textAlign='center'>Login</Typography>
 							</Link>
-						) : (
-							<Button variant='default' onClick={signUserOut}>
-								Log Out
-							</Button>
 						)}
 					</Box>
 
 					<Box sx={{ flexGrow: 0 }}>
-						<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-							<Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
-						</IconButton>
+						<Tooltip title='More options'>
+							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+							</IconButton>
+						</Tooltip>
+						{isAuth && (
+							<Menu
+								sx={{ mt: '45px' }}
+								id='menu-appbar'
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								<MenuItem onClick={signUserOut}>
+									<Typography textAlign='center'>Log Out</Typography>
+								</MenuItem>
+							</Menu>
+						)}
 					</Box>
 				</Toolbar>
 			</Container>
