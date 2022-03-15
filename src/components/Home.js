19896fcfebe85272c90a45b2b5b2d41939c6fd9d
Peter Kang <peeterkang@gmail.com>
2022-03-14 20:19:03 -0700

    move homestyles into dedicated file

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
 import { auth, db } from '../firebase-config';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Grid, Typography, IconButton } from '@mui/material';
+import { postStyles, textStyles, containerStyles} from './HomeStyles';
 
 const Home = ({ isAuth }) => {
 	const [posts, setPosts] = useState([]);
@@ -22,37 +22,13 @@ const Home = ({ isAuth }) => {
 		getPosts();
 	}, [deletePost]);
 
-	const postStyles = {
-		minWidth: '370px',
-		width: '600px',
-		height: 'auto',
-		maxHeight: '600px',
-		backgroundColor: 'rgb(250, 250, 250)',
-		boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
-		margin: '20px',
-		padding: '20px',
-		borderRadius: '15px',
-	};
-
-	const textStyles = {
-		wordWrap: 'break-word',
-		height: 'auto',
-		maxHeight: '400px',
-		width: '100%',
-		overflow: 'hidden',
-		overflowY: 'auto',
-	};
-
 	return (
 		<Grid
 			container
 			direction='column'
 			justifyContent='center'
 			alignItems='center'
-			sx={{
-				height: 'auto',
-				minHeight: 'calc(100vh - 80px)',
-			}}
+			sx={containerStyles}
 		>
 			{posts.map(({ id, title, post, author }) => (
 				<Grid key={id} item container sx={postStyles}>
---
+++ src/components/HomeStyles.js
@@ -0,0 +1,25 @@
+export const postStyles = {
+    minWidth: '370px',
+    width: '600px',
+    height: 'auto',
+    maxHeight: '600px',
+    backgroundColor: 'rgb(250, 250, 250)',
+    boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+    margin: '20px',
+    padding: '20px',
+    borderRadius: '15px',
+};
+
+export const textStyles = {
+    wordWrap: 'break-word',
+    height: 'auto',
+    maxHeight: '400px',
+    width: '100%',
+    overflow: 'hidden',
+    overflowY: 'auto',
+};
+
+export const containerStyles = {
+    height: 'auto',
+    minHeight: 'calc(100vh - 80px)',
+}
