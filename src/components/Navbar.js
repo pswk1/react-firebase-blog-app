9d835ebbccec938efa7f816cc05845443ba58314
Peter Kang <peeterkang@gmail.com>
2022-03-17 14:42:29 -0700

    refactor: navbar bg color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,7 +34,11 @@ const Navbar = ({ isAuth, signUserOut }) => {
 	};
 
 	return (
-		<AppBar position='static'>
+		<AppBar position='static'
+		sx={{
+			background: 'linear-gradient(90deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
+		}}
+		>
 			<Container maxWidth='xl'>
 				<Toolbar disableGutters>
 					<Typography
